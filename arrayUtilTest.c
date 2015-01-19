33c92c535c2d3da7f5291bf93ebadcb5c3553091
Mahesh K <mahesh.mk.245@gmail.com>
2015-01-19 17:54:36 +0530

    areEqaul and create functions are completed

--- arrayUtilTest.c
+++ arrayUtilTest.c
@@ -5,36 +5,36 @@
 
 void test_areEqual_returns_1_if_both_are_equal(){
 	int equal;
-	ArrayUtil a;
-	ArrayUtil b;
-	a.length = 2;
-	b.length = 2;
-	a.typeSize =4;
-	b.typeSize =4;
-	a.base = (int *)a.base;
-	b.base = (int *)b.base;
-	((int *)a.base)[0] = 1;
-	((int *)a.base)[1] = 2;
-	((int *)b.base)[0] = 1;
-	((int *)b.base)[1] = 2;
+	ArrayUtil a = create(4, 2);
+	ArrayUtil b = create(4, 2);
+	int *aBase = (int *)(a.base);
+	int *bBase = (int *)(b.base);
+	aBase[0] = 1;
+	aBase[1] = 2;
+	bBase[0] = 1;
+	bBase[1] = 2;
 	equal = areEqual(a, b);
 	assertEqual(equal, 1);
 } 
 
 void test_areEqual_returns_0_if_both_are_not_equal(){
 	int equal;
-	ArrayUtil a;
-	ArrayUtil b;
-	a.length = 2;
-	b.length = 2;
-	a.typeSize =4;
-	b.typeSize =4;
-	a.base = (int *)a.base;
-	b.base = (int *)b.base;
-	((int *)a.base)[0] = 1;
-	((int *)a.base)[1] = 1;
-	((int *)b.base)[0] = 1;
-	((int *)b.base)[1] = 2;
+	ArrayUtil a = create(4, 2);
+	ArrayUtil b = create(4, 2);
+	int *aBase = (int *)(a.base);
+	int *bBase = (int *)(b.base);
+	aBase[0] = 1;
+	aBase[1] = 1;
+	bBase[0] = 1;
+	bBase[1] = 2;						
 	equal = areEqual(a, b);
 	assertEqual(equal, 0);
+} 
+
+void test_areEqual_returns_1_if_both_arrays_are_not_intialized(){
+	int equal;
+	ArrayUtil a = create(4, 2);
+	ArrayUtil b = create(4, 2);
+	equal = areEqual(a, b);
+	assertEqual(equal, 1);
 } 
